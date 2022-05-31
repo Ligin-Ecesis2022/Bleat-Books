c1bdb88fb43d7a549483a0af1c8b0616d2dbea67
Ligin-Ecesis2022 <liginjohnecesis@gmail.com>
2022-05-31 18:39:36 +0530

    31-05

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
-$(document).ready(function() {
-    $(".accordian-title").click(function(){
-        $(this).parent('.accordian').toggleClass('open');
-        // $(".accordian").siblings().hasClass("open").remove;
-    }); 
+// $(document).ready(function() {
+//     $(".accordian-title").click(function(){
+//         $(this).parent('.accordian').toggleClass('open');
+//         // $(".accordian").siblings().hasClass("open").remove;
+//     }); 
 
+// });
+
+
+
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+
+		// Variables privadas
+		var links = this.el.find('.accordian-title');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.accordian-text').not($next).slideUp().parent().removeClass('open');
+		};
+	}	
+
+	var accordion = new Accordion($('.accordian'), false);
 });
+
 
   
